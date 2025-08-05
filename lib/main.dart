bd0802762e202f61604ebf5b013b70dc7e902410
Abid <abidroid1@gmail.com>
2025-08-05 21:50:03 +0500

    name concate

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_online_june_project/screens/dynamic_listview_screen.dart
 import 'package:flutter_online_june_project/screens/first_screen.dart';
 import 'package:flutter_online_june_project/screens/images_demo_screen.dart';
 import 'package:flutter_online_june_project/screens/listview_demo_screen.dart';
+import 'package:flutter_online_june_project/screens/name_concat_screen.dart';
 import 'package:flutter_online_june_project/screens/row_and_column_demo_screen.dart';
 import 'package:flutter_online_june_project/screens/simple_gridview_screen.dart';
 import 'package:flutter_online_june_project/screens/stack_demo_screen.dart';
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const TextfieldDemoScreen() // Launcher screen
+      home: const NameConcatScreen() // Launcher screen
     );
   }
 }
---
+++ lib/screens/name_concat_screen.dart
@@ -0,0 +1,92 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_online_june_project/models/doctor.dart';
+import 'package:gap/gap.dart';
+
+class NameConcatScreen extends StatefulWidget {
+  const NameConcatScreen({super.key});
+
+  @override
+  State<NameConcatScreen> createState() => _NameConcatScreenState();
+}
+
+class _NameConcatScreenState extends State<NameConcatScreen> {
+
+  // initial Value
+  String fullName = "Fullnama";
+
+
+  var firstC = TextEditingController();
+  var lastC = TextEditingController();
+  FocusNode myFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Name Concate'),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            TextField(
+              controller: firstC,
+              focusNode: myFocusNode,
+              decoration: InputDecoration(
+                hintText: 'First name',
+                border: OutlineInputBorder()
+              ),
+            ),
+            Gap(20),
+            TextField(
+              controller: lastC,
+              decoration: InputDecoration(
+                hintText: 'Last name',
+                border: OutlineInputBorder()
+            ),),
+
+            Gap(20),
+            Row(
+              spacing: 20,
+              children: [
+                ElevatedButton(onPressed: (){
+
+                  firstC.clear();
+                  lastC.clear();
+
+                  setState(() {
+                    fullName = '';
+                  });
+
+                  myFocusNode.requestFocus();
+
+                }, child: const Text('Clear')),
+                ElevatedButton(onPressed: (){
+                  // combine logic
+                  // 1. get text from first name tf
+                    String firstName =  firstC.text;
+
+                  // 2. get text from second name tf
+
+                  String lastName = lastC.text;
+                  // 3. concate
+
+                  setState(() {
+                    fullName = '$firstName $lastName';
+                  });
+                }, child: const Text('Combine')),
+              ],
+            ),
+            Gap(20),
+
+            Text(fullName),
+          ],
+        ),
+      ),
+    );
+  }
+}
